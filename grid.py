0cc3ea860c83d67b5313a61ac5db459be5273382
nholler3 <98353433+nholler3@users.noreply.github.com>
2024-03-15 22:25:33 -0400

    tighten up

--- grid.py
+++ grid.py
@@ -2,12 +2,12 @@ import random as rand
 
 #The same number cannot exist in the same row, column, or cluster
 
-def fillList(): #use the rules of sudoku to fill in the 2d array use helper methods
+def fillGrid(): #use the rules of sudoku to fill in the 2d array use helper methods
     sudoku=[[rand.randint(1,9) for _ in range(9)] for _ in range(9)]
     rowRule(sudoku)
     return sudoku
 
-def rowRule(sudoku):
+def rowRule(sudoku): #replaces each duplicate number in a row with 0
     for row in range(len(sudoku)): #each row in sudoku
         for col in range(len(sudoku[0])): # each column number in the row
             temp=sudoku[row][col] #temporary number holding the position of row and columns
@@ -18,7 +18,7 @@ def rowRule(sudoku):
                         sudoku[row][i]=0
     return sudoku
 
-def colRule():
+def colRule(sudoku):
     print()
 
 def clusterRule():
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #runs the files
 
 from board import makeBoard
-from filler import fillList
+from grid import fillGrid
 
 nums = [[1,2,3,4,5,6,7,8,9,],[2,3,4,5,6,7,8,9,1],[3,4,5,6,7,8,9,1,2],
         [1,2,3,4,5,6,7,8,9,],[2,3,4,5,6,7,8,9,1],[3,4,5,6,7,8,9,1,2],
-        [1,2,3,4,5,6,7,8,9,],[2,3,4,5,6,7,8,9,1],[3,4,5,6,7,8,9,1,2]]
+        [1,2,3,4,5,6,7,8,9,],[2,3,4,5,6,7,8,9,1],[3,4,5,6,7,8,9,1,2]]# used to test 
 
-fill=fillList()
+fill=fillGrid()
 
-makeBoard(nums)
+makeBoard(nums)#make sure boarder is handling the entire 2d list
 print()
 makeBoard(fill)
