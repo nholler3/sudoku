8047dcd1232e134d2527fc28c998f099e18bce34
nholler3 <98353433+nholler3@users.noreply.github.com>
2024-03-22 22:58:59 -0400

    helpers for backtracking

--- grid.py
+++ grid.py
@@ -1,13 +1,14 @@
 import random as rand
-from board import makeBoard #test purposes
+
+from board import makeBoard  # test purposes
 
 #The same number cannot exist in the same row, column, or cluster
 
-def fillGrid(): #use the rules of sudoku to fill in the 2d array use helper methods
+""" def fillGrid(): #use the rules of sudoku to fill in the 2d array use helper methods
     sudoku=[[rand.randint(1,9) for _ in range(9)] for _ in range(9)]
     validateGrid(sudoku,0, 0)
     solveGrid(sudoku, 0, 0)
-    return sudoku
+    return sudoku """
 
 def fillGrid2(): #does not require rowRule (is it more efficient in the long run?) most likely yes, less 0s to fill in
     sudoku = [[0 for _ in range(9)] for _ in range(9)]  # Initialize a 9x9 grid with zeros
@@ -20,20 +21,23 @@ def fillGrid2(): #does not require rowRule (is it more efficient in the long run
             num = validNums.pop()  # Take the last element from the shuffled list
             sudoku[row][col] = num
 
-    makeBoard(sudoku)#test purposes
+    #makeBoard(sudoku)#test purposes
     validateGrid(sudoku, 0, 0)
-    solveGrid(sudoku, 0, 0)
+    makeBoard(sudoku)
+    solveGrid(sudoku, 0, 0, 1)
     return sudoku
 
-def rowRule(sudoku): #replaces each duplicate number in a row with 0
+""" def rowRule(sudoku): #replaces each duplicate number in a row with 0
     for row in range(len(sudoku)): #each row in sudoku
         for col in range(len(sudoku[0])): # each column number in the row
             temp=sudoku[row][col] #temporary number holding the position of row and columns
             if temp!= 0:
                 for i in range(col+1,len(sudoku[0])):#check the rest of the values in the row
                     if temp == sudoku[row][i]:#replace the duplicate with 0
-                        sudoku[row][i]=0
-    return sudoku
+                        #sudoku[row][i]=0
+                        return True
+                    return False
+    return sudoku """
 
 def colRule(sudoku):
     for row in range(len(sudoku)): #each row in sudoku
@@ -48,12 +52,12 @@ def clusterRule(sudoku):
     print()
 
 def validateGrid(sudoku, row, col):
-    rowRule(sudoku)
+    #rowRule(sudoku)
     colRule(sudoku)
     #clusterRule(sudoku)
     #solveGrid(sudoku, row, col)
     
-def solveGrid(sudoku, row, col):
+def solveGrid(sudoku, row, col, num):
     #logic for replacing the number and recursive call
     #back tracking algorithm
     if row >= len(sudoku)-1 and col > len(sudoku)-1:#if we hit the end of the final list then exit
@@ -65,8 +69,35 @@ def solveGrid(sudoku, row, col):
     #beginning of backtracking
     if sudoku[row][col] != 0: #if the index is not empty call again with the next column
         #print("(",row,",",col, ") filled")     #test purposes
-        return solveGrid(sudoku, row, col+1)
+        num=1
+        return solveGrid(sudoku, row, col+1, num)
     else: #if the number is 0
-        sudoku[row][col]='*' #replace with the * (for tracking purposes)
+
+        replaced = [[]] # Initialize a list to hold the visited coordinates (for backtracking)
+
+        if rowCheck(sudoku, row, num)==True:
+            print("num count at 0: ","(",row,",",col, ")",num)
+            sudoku[row][col]=num
+        else:
+            num+=1
+
+        #sudoku[row][col]='*' #replace with the * (for tracking purposes)
         #print("(",row,",",col, ") hit")        #test purposes
-        return solveGrid(sudoku, row, col+1)
+
+        return solveGrid(sudoku, row, col+1, num) #recursive call for next cell
+    
+def rowCheck(sudoku, row, target): #replaces each duplicate number in a row with 0
+        for col in range(len(sudoku[0])): # each column number in the row
+            temp=sudoku[row][col] #temporary number holding the position of row and columns
+            if target in sudoku[row]:
+                return False
+            return True
+        
+def colCheck(sudoku):
+    for row in range(len(sudoku)): #each row in sudoku
+        for col in range(len(sudoku[0])): # each column number in the row
+            temp=sudoku[row][col] #temporary number holding the position of current row and column
+            if temp!= 0:
+                for i in range(row+1,len(sudoku[0])):#check the rest of the values in the column
+                    if temp == sudoku[i][col]:#replace the duplicate with 0
+                        sudoku[i][col]=0
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #runs the files
 
 from board import makeBoard
-from grid import fillGrid
+#from grid import fillGrid
 from grid import fillGrid2
 
 nums = [[1,2,3,4,5,6,7,8,9,],[2,3,4,5,6,7,8,9,1],[3,4,5,6,7,8,9,1,2],
         [1,2,3,4,5,6,7,8,9,],[2,3,4,5,6,7,8,9,1],[3,4,5,6,7,8,9,1,2],
         [1,2,3,4,5,6,7,8,9,],[2,3,4,5,6,7,8,9,1],[3,4,5,6,7,8,9,1,2]]# used to test 
 
-fill=fillGrid()
+#fill=fillGrid()
 
 #print("nums board:")
 #makeBoard(nums)#make sure boarder is handling the entire 2d list
